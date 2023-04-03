1a3b8ddacb5c2842a75ce496bb3d558f8a5354e6
janetpaul00 <84874007+janetpaul00@users.noreply.github.com>
2023-04-04 00:35:48 +0400

    using map function

--- src/Exp.jsx
+++ src/Exp.jsx
@@ -2,49 +2,57 @@ import ISHOF from "./assets/ISHOF.png";
 import WWF_logo from "./assets/WWF_logo.svg.png";
 
 export default function Experience() {
+  let data = [
+    {
+      image: ISHOF,
+      alt: "logo of International Swimming Hall of Fame",
+      height: 399,
+      width: 436,
+      datefrom: "2023-02-01",
+      datefrom_text: "Feb 2023",
+      dateto: "2023-03-31",
+      datetotext: "Current",
+      title: "Swim Coach",
+      about:
+        "Taught humans of all ages to swim for rehabilitation, competitive sports and stress relief",
+    },
+    {
+      image: WWF_logo,
+      alt: "logo of WWF",
+      height: 177,
+      width: 120,
+      datefrom: "2022-02-01",
+      datefrom_text: "Feb 2022",
+      dateto: "2020-03-31",
+      datetotext: "March 2020",
+      title: "Wildlife Conservationist",
+      about:
+        "Used my knowledge of animal behavior and habitats to protect endangered species and promote conservation efforts.",
+    },
+  ];
+
   return (
     <div className="experiences">
       <h2>Experience</h2>
-      <div className="experience">
-        <img
-          className="logo"
-          src={ISHOF}
-          alt="logo of International Swimming Hall of Fame"
-          height="399"
-          width="436"
-        />
-        <div className="exp-container">
-          <div className="period">
-            <time dateTime="2022-02-01">Feb 2023</time> -
-            <time dateTime="2023-03-31">Current</time>
-          </div>
-          <div className="role">Swim Coach</div>
-          <div className="role-about">
-            Taught humans of all ages to swim for rehabilitation, competitive
-            sports and stress relief.
-          </div>
-        </div>
-      </div>
-      <div className="experience">
-        <img
-          className="logo"
-          src={WWF_logo}
-          alt="logo of WWF"
-          height="177"
-          width="120"
-        />
-        <div className="exp-container">
-          <div className="period">
-            <time dateTime="2022-02-01">Feb 2022</time> -
-            <time dateTime="2020-03-31">March 2020</time>
-          </div>
-          <div className="role">Wildlife Conservationist</div>
-          <div className="role-about">
-            Used my knowledge of animal behavior and habitats to protect
-            endangered species and promote conservation efforts.
+      {data.map((role, index) => (
+        <div className="experience" key={index}>
+          <img
+            className="logo"
+            src={role.image}
+            alt={role.alt}
+            height={role.height}
+            width={role.width}
+          />
+          <div className="exp-container">
+            <div className="period">
+              <time dateTime={role.datefrom}>{role.datefrom_text}</time> -
+              <time dateTime={role.dateto}>{role.datetotext}</time>
+            </div>
+            <div className="role">{role.title}</div>
+            <div className="role-about">{role.about}</div>
           </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 }
--- src/Skills.jsx
+++ src/Skills.jsx
@@ -1,74 +1,50 @@
 export default function Skills() {
+  let skills_data = [
+    {
+      skill_name: "Waddle",
+      value: 98,
+    },
+    {
+      skill_name: "Diver",
+      value: 80,
+    },
+    {
+      skill_name: "Navigation",
+      value: 50,
+    },
+    {
+      skill_name: "Nest Building",
+      value: 60,
+    },
+    {
+      skill_name: "Public Quacking",
+      value: 85,
+    },
+    {
+      skill_name: "Duck Yoga",
+      value: 90,
+    },
+  ];
+
   return (
     <div className="skills">
       <h2>Skills</h2>
       <ul>
-        <li>
-          <p className="skill">Waddle</p>
-          <progress
-            value="98"
-            max="100"
-            style={{
-              width: "60%",
-              height: "25px",
-            }}
-          />
-        </li>
-        <li>
-          <p className="skill">Diver</p>
-          <progress
-            value="80"
-            max="100"
-            style={{
-              width: "60%",
-              height: "25px",
-            }}
-          />
-        </li>
-        <li>
-          <p className="skill">Navigation</p>
-          <progress
-            value="50"
-            max="100"
-            style={{
-              width: "60%",
-              height: "25px",
-            }}
-          />
-        </li>
-        <li>
-          <p className="skill">Nest Building</p>
-          <progress
-            value="60"
-            max="100"
-            style={{
-              width: "60%",
-              height: "25px",
-            }}
-          />
-        </li>
-        <li>
-          <p className="skill">Quacking</p>
-          <progress
-            value="80"
-            max="100"
-            style={{
-              width: "60%",
-              height: "25px",
-            }}
-          />
-        </li>
-        <li>
-          <p className="skill">Duck Yoga</p>
-          <progress
-            value="90"
-            max="100"
-            style={{
-              width: "60%",
-              height: "25px",
-            }}
-          />
-        </li>
+        {skills_data.map((skill, index) => (
+          <li>
+            <p className="skill" key={index}>
+              {skill.skill_name}
+            </p>
+            <progress
+              value={skill.value}
+              max="100"
+              style={{
+                width: "60%",
+                height: "25px",
+              }}
+            />{" "}
+          </li>
+        ))}
       </ul>
     </div>
   );
