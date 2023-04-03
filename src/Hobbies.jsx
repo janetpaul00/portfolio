ed6a0efaa2ed6f9044154b66d07194676eb363f6
janetpaul00 <84874007+janetpaul00@users.noreply.github.com>
2023-04-03 16:44:26 +0400

    Update Hobbies.jsx

--- src/Hobbies.jsx
+++ src/Hobbies.jsx
@@ -3,31 +3,33 @@ import standup from "./assets/standup.jpg";
 import skydiving from "./assets/skydiving.jpg";
 
 export default function Hobbies() {
+  let hobby_data = [
+    {
+      alt: "eggs and butter",
+      image: baking,
+      title: "Baking",
+      description:
+        "Check out my sourdough and other recipes that use duck eggs in creative ways!",
+    },
+    {
+      alt: "close up of a mic",
+      image: standup,
+      title: "Stand-up comedy",
+      description:
+        "My take on the challenges of being a duck in this human centric world.",
+    },
+    {
+      alt: "duck flying under the clear blue sky",
+      image: skydiving,
+      title: "Skydiving",
+      description: "Come see my daring stunts during free-fall!",
+    },
+  ];
+
   return (
     <div className="hobbies">
       <h2>Hobbies</h2>
-      {[
-        {
-          alt: "eggs and butter",
-          image: baking,
-          title: "Baking",
-          description:
-            "Check out my sourdough and other recipes that use duck eggs in creative ways!",
-        },
-        {
-          alt: "close up of a mic",
-          image: standup,
-          title: "Stand-up comedy",
-          description:
-            "My take on the challenges of being a duck in this human centric world.",
-        },
-        {
-          alt: "duck flying under the clear blue sky",
-          image: skydiving,
-          title: "Skydiving",
-          description: "Come see my daring stunts during free-fall!",
-        },
-      ].map((hobby, index) => (
+      {hobby_data.map((hobby, index) => (
         <div className="hobby" key={index}>
           <img
             className="hobby-img"
